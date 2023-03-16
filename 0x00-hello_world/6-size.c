ceb2d9b08c1e2f6263dc6732329a70a1ff3b3ee7
Adnane-Bahili <AdnaneBahili2@gamil.com>
2023-03-16 12:59:07 -0700

    Project

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main () {
+	char c;
+	int i;
+	long int li;
+	long long int lli;
+	float f;
+	printf("the size of a char: %ld byte(s).\n", sizeof(c));
+	printf("the size of an int: %ld byte(s).\n", sizeof(i));
+        printf("the size of a long int: %ld byte(s).\n", sizeof(li));
+        printf("the size of a long long int: %ld byte(s).\n", sizeof(lli));
+        printf("the size of a float: %ld byte(s).\n", sizeof(f));
+        return(0);
+}
