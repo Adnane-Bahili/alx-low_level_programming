bf8e574f05e7fcabe05ad7f51adbe574e527c1f4
Adnane-Bahili <AdnaneBahili2@gamil.com>
2023-03-16 13:19:41 -0700

    Betty fix + 0. typo

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,16 @@
 #include<stdio.h>
-int main () {
+int main(void)
+{
 	char c;
 	int i;
 	long int li;
 	long long int lli;
 	float f;
+
 	printf("the size of a char: %ld byte(s).\n", sizeof(c));
 	printf("the size of an int: %ld byte(s).\n", sizeof(i));
-        printf("the size of a long int: %ld byte(s).\n", sizeof(li));
-        printf("the size of a long long int: %ld byte(s).\n", sizeof(lli));
-        printf("the size of a float: %ld byte(s).\n", sizeof(f));
-        return(0);
+	printf("the size of a long int: %ld byte(s).\n", sizeof(li));
+	printf("the size of a long long int: %ld byte(s).\n", sizeof(lli));
+	printf("the size of a float: %ld byte(s).\n", sizeof(f));
+	return (0);
 }
