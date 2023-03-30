f9e4cdf1351678ee401782c79d92f4d220c0f7f7
Adnane-Bahili <AdnaneBahili2@gmail.com>
2023-03-30 19:54:47 +0000

    Advanced tasks

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_number - prints an integer
+ * @n: integer to print
+ *
+ * Return: void
+ */
+void print_number(int n)
+{
+	unsigned int i = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		i = -i;
+	}
+	if (i > 9)
+		print_number(i / 10);
+	_putchar((i % 10) + '0');
+}
---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_buffer - prints a buffer, formatted
+ *
+ * @b: buffer pointer
+ * @size: number of bytes to buffer 
+ * Return: void
+ */
+void print_buffer(char *b, int size)
+{
+	int i, j = 0, k = size;
+
+	if (size % 10 != 0)
+		k = ((size / 10) + 1) * 10;
+	if (size <= 0)
+	{
+		printf("\n");
+		return;
+	}
+	for (i = 0; i < k; i++)
+	{
+		if (j++ % 10 == 0)
+		{
+			printf("%08x: ", i);
+		}
+		if (i < size)
+			printf("%02x", *(b + i));
+		else
+			printf("  ");
+		if (i % 2 != 0)
+			printf(" ");
+		if (j == 10)
+		{
+			for (j = i - 9; j <= i; j++)
+			{
+				if (*(b + j) > 31 && j < size)
+					printf("%c", *(b + j));
+				else if (j < size)
+					printf(".");
+			}
+			printf("\n");
+			j = 0;
+		}
+	}
+}
