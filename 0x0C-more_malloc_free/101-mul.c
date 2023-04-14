451b9dcff23ab4a3ae59e3faea8e0023aaf1aeca
Adnane-Bahili <125834925+Adnane-Bahili@users.noreply.github.com>
2023-04-14 02:10:29 +0000

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,89 +1,134 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include "main.h"
+void *_calloc(unsigned int nmemb, unsigned int size)
+char *_memset(char *s, char b, unsigned int n)
+int _strlen(char *s)
+int Numbers(char *c)
+void multiply(char *a, char *b)
 
-int is_number(char *s)
+
+/**
+ * main - multiplies two positive numbers
+ * @argc: argument count
+ * @argv: argument vectors
+ * Return: 0
+ */
+int main(int argc, char *argv[])
 {
-    int i;
-    
-    for (i = 0; s[i]; i++)
-    {
-        if (!(s[i] >= '0' && s[i] <= '9'))
-            return (0);
-    }
-    return (1);
+	char *a = argv[1];
+	char *b = argv[2];
+
+	if (argc != 3 || !Numbers(a) || !Numbers(b))
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (*a == 48 || *b == 48)
+		printf("0\n");
+	else
+		multiply(b, a);
+	return (0);
 }
 
-void error(void)
+/**
+ * multiply - multiplies 
+ * @a: 1st
+ * @b: 2nd
+ */
+void multiply(char *a, char *b)
 {
-    printf("Error\n");
-    exit(98);
+	int i, l1, l2, total, adigit, bdigit, result = 0, t;
+	int *p;
+
+	l1 = _strlen(a);
+	l2 = _strlen(b);
+	t = l2;
+	total = l1 + l2;
+	p = _calloc(sizeof(int), (l1 + l2));
+	for (l1--; l1 >= 0; l1--)
+	{
+		adigit = a[l1] - '0';
+		result = 0;
+		l2 = t;
+		for (l2--; l2 >= 0; l2--)
+		{
+			bdigit = b[l2] - '0';
+			result += p[l2 + l1 + 1] + (adigit * bdigit);
+			p[l1 + l2 + 1] = result % 10;
+			result /= 10;
+		}
+		if (result)
+			p[l1 + l2 + 1] = result % 10;
+	}
+	while (*p == 0)
+	{
+		p++;
+		total--;
+	}
+	for (i = 0; i < total; i++)
+		printf("%i", p[i]);
+	printf("\n");
+}
+/**
+ * Numbers - determines
+ * @c: input 
+ * Return: 0 if false
+ * 1 if true
+ */
+int Numbers(char *c)
+{
+	while (*c)
+	{
+		if (*c < '0' || *c > '9')
+			return (0);
+		c++;
+	}
+	return (1);
 }
 
-int str_len(char *str)
+/**
+ * _strlen - returns 
+ * @s: s
+ * Return: length of string
+ */
+int _strlen(char *s)
 {
-    int i = 0;
+	char *p = s;
 
-    while (str[i])
-        i++;
-    return (i);
+	while (*s)
+		s++;
+	return (s - p);
 }
 
-void compute_mult(int len1, int len2, char *s1, char *s2, int *result)
+/**
+ * _memset - fills
+ * @s: mem area
+ * @b: const byte
+ * @n: bytes of mem area
+ * Return: pointer 
+ */
+char *_memset(char *s, char b, unsigned int n)
 {
-    int carry = 0;
-    int digit1 = 0;
-    int digit2 = 0;
-    int i, j;
+	char *p = s;
 
-    for (i = 0; i < len1 + len2 + 1; i++)
-        result[i] = 0;
-
-    for (i = len1 - 1; i >= 0; i--)
-    {
-        digit1 = s1[i] - '0';
-        carry = 0;
-        for (j = len2 - 1; j >= 0; j--)
-        {
-            digit2 = s2[j] - '0';
-            carry += result[i + j + 1] + (digit1 * digit2);
-            result[i + j + 1] = carry % 10;
-            carry /= 10;
-        }
-        if (carry > 0)
-            result[i] = carry;
-    }
+	while (n--)
+		*s++ = b;
+	return (p);
 }
 
-int main(int ac, char *av[])
+/**
+ * _calloc - allocates 
+ * @nmemb: elements of pointer
+ * @size: size 
+ * Return: pointer 
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
 {
-    int len1 = 0, len2 = 0, *result;
-    char *s1, *s2;
-    int checker = 0;
-    int i;
+	void *p;
 
-    if (ac != 3)
-        error();
-    if (!(is_number(av[1]) && is_number(av[2])))
-        error();
-    s1 = av[1];
-    s2 = av[2];
-    len1 = str_len(s1);
-    len2 = str_len(s2);
-    result = malloc(sizeof(int) * (len1 + len2 + 1));
-    for (i = 0; i < len1 + len2 + 1; i++)
-        result[i] = 0;
-    compute_mult(len1, len2, s1, s2, result);
-    for (i = 0; i < len1 + len2; i++)
-    {
-        if (result[i])
-            checker = 1;
-        if (checker)
-            putchar(result[i] + 48);
-    }
-    if (checker == 0)
-        _putchar('0');
-    _putchar('\n');
-    free(result);
-    return (0);
+	if (!nmemb || !size)
+		return (NULL);
+	p = malloc(size * nmemb);
+	if (!p)
+		return (NULL);
+	_memset(p, 0, size * nmemb);
+	return (p);
 }
