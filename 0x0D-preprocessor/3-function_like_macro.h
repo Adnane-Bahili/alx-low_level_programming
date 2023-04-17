0291cce06d3dc34df21891f12d99b2fc9e54ba54
Adnane-Bahili <AdnaneBahili2@gmail.com>
2023-04-17 21:01:56 +0000

    Complete Project

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,8 @@
+#ifndef _3_FUNCTION_LIKE_MACRO_H
+#define _3_FUNCTION_LIKE_MACRO_H
+
+
+#define ABS(i) ((i) < (0) ? -(i) : (i))
+
+
+#endif // _3_FUNCTION_LIKE_MACRO_H
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,8 @@
+#ifndef _4_SUM_H
+#define _4_SUM_H
+
+
+#define SUM(i, j) ((i) + (j))
+
+
+#endif // _4_SUM_H
